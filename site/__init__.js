56ad150042d6cec10f776cb3570cc53efab4cc38
NeoAtlantis <rijndael@yeah.net>
2013-11-26 16:34:56 +0800

    add expression reader

--- site/__init__.js
+++ site/__init__.js
@@ -1,3 +1,22 @@
+var router = [
+    [/^\/identity\/search\/id\/?$/,],
+    [/^\/identity\/search\/keyword\/?$/, ],
+    [/^\/codebook\/add\/?$/, ],
+    [/^\/codebook\/edit\/?$/, ],
+    [/^\/codebook\/remove\/?$/, ],
+    [/^\/codebook\/query\/?$/, ],
+
+];
+
 module.exports = function(packet){
-    
+    var workflow = [];
+    var database = $.global.get('database');
+
+    database.table('identity').select(
+        [{'id': 1, 'p': {$gt: 2, $lt: 10}}, {'id':3}]
+    );
+
+    $.nodejs.async.waterfall(workflow, function(err, result){
+    packet.end('');
+    });
 };

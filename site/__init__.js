7318b3def0b6b531c23dbf41165fbdb7f9e8ffdc
NeoAtlantis <rijndael@yeah.net>
2013-11-27 02:22:30 +0800

    save

--- site/__init__.js
+++ site/__init__.js
@@ -1,20 +1,74 @@
 var router = [
-    [/^\/identity\/search\/id\/?$/,],
-    [/^\/identity\/search\/keyword\/?$/, ],
-    [/^\/codebook\/add\/?$/, ],
-    [/^\/codebook\/edit\/?$/, ],
-    [/^\/codebook\/remove\/?$/, ],
-    [/^\/codebook\/query\/?$/, ],
-
+    [/^\/identity\/query\/([0-9a-fA-F])+\/?$/,
+                                            require('./identity.js'),   'DB',],
+    [/^\/identity\/search\/keyword\/?$/,    require('./identity.js'),   'DB',],
+    [/^\/codebook\/(add|edit|remove)\/?$/,  require('./codebook.js'),   'DB', 'AUTH', 'POST'],
+    [/^\/codebook\/query\/([0-9a-fA-F]+)\/?$/,
+                                            require('./codebook.js'),   'DB',],
+    [/^\/?$/,                               require('./landmark.js'), ],
 ];
 
 module.exports = function(packet){
-    var workflow = [];
-    var database = $.global.get('database');
+    var workflow = [], result = null, routerRule = null, handler = null;
+    var database = null;//$.global.get('database');
 
-    database.table('identity').insert({id: 'abcdefg', data: 'abcdefghijklmn"=\''});
+    for(var i in router){
+        routerRule = router[i];
+        result = routerRule[0].exec(packet.request.url);
+        if(null != result) break;
+    };
 
-    $.nodejs.async.waterfall(workflow, function(err, result){
-    packet.end('');
-    });
+    if(null == result){
+        packet.response.writeHead(400);
+        packet.response.end();
+        return;
+    };
+
+    handler = routerRule[1];
+    routerRule = routerRule.slice(2);
+
+    // check database
+    if(routerRule.indexOf('DB') >= 0){
+        workflow.push(function(callback){
+            if(null == database)
+                callback(503);
+            else
+                callback(null);
+        });
+    };
+
+    // check auth
+    if(routerRule.indexOf('AUTH') >= 0){
+        workflow.push(function(callback){
+            // XXX do something
+            callback(401);
+        });
+    };
+
+    function startWork(post){
+        workflow.push(function(callback){
+            handler(packet, result, post, callback);
+        });
+        $.nodejs.async.waterfall(workflow, function(err, result){
+            if(null == err){
+                packet.response.writeHead(200);
+                packet.response.end(result);
+            } else {
+                packet.response.writeHead(err);
+                packet.response.end();
+            };
+        });
+    };
+
+    // wait post data, if needed
+    if(routerRule.indexOf('POST') >= 0){
+        if(packet.method != 'post'){
+            packet.response.writeHead(405);
+            packet.response.end();
+        } else {
+            packet.on('ready', startWork);
+        };
+    } else {
+        startWork(null);
+    };
 };
---
+++ site/codebook.js
@@ -0,0 +1,8 @@
+module.exports = function(packet, result, post, callback){
+    var action = result[1],
+        workflow = [];
+
+    if(['add', 'edit', 'remove'].indexOf(action) >= 0){
+    } else {
+    };
+};
